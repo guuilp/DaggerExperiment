9fe3c0b8c0078a8b060dbba31a65c3397048a351
Guilherme Pereira <guilherme.pereira@ifood.com.br>
2021-02-25 10:41:53 -0300

    Removing unnecessary component build

--- app/src/main/java/com/github/guuilp/daggerexperiment/di/core/MultiBindingViewModelFactory.kt
+++ app/src/main/java/com/github/guuilp/daggerexperiment/di/core/MultiBindingViewModelFactory.kt
@@ -10,14 +10,11 @@ import javax.inject.Provider
 typealias ViewModelMap = Map<Class<out ViewModel>, @JvmSuppressWildcards Provider<ViewModel>>
 
 class MultiBindingViewModelFactory @Inject constructor(
-    private val map: ViewModelMap,
-    private val viewModelComponent: Provider<ViewModelComponent.Builder>
+    private val map: ViewModelMap
 ) : ViewModelProvider.Factory {
 
     @Suppress("UNCHECKED_CAST")
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-        viewModelComponent.get().build()
-
         return map[modelClass]?.get() as? T?
             ?: throw IllegalStateException("Could not create ViewModel of type $modelClass")
     }
